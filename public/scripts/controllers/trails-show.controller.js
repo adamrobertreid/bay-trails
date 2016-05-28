95e32303bdfd936aae44ac8ccba06af3a4400610
adamrobertreid <adamrobertreid@gmail.com>
2016-05-28 15:30:58 -0700

    trails show controller get request

---
+++ public/scripts/controllers/trails-show.controller.js
@@ -0,0 +1,25 @@
+TrailsShowController.$inject = ["$location", "$http", "$routeParams"]; // minification protection
+function TrailsShowController ($location, $http, $routeParams) {
+  var vm = this;
+  vm.trail = {};
+
+  var id = $routeParams.id;
+  get(); // fetch one trail (show)
+
+  ////
+
+  function get() {
+    $http
+      .get('/api/trails/' + id)
+      .then(onGetSuccess, onGetError);
+
+    function onGetSuccess(response){
+      vm.trail = response.data;
+    }
+
+    function onGetError(response){
+      console.error("Failed to get trail", response);
+      $location.path("/");
+    }
+  };
+}

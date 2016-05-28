9820cb244e3760040c99af52c131a3302d4c430e
adamrobertreid <adamrobertreid@gmail.com>
2016-05-28 15:32:44 -0700

    trails-new controller create made

---
+++ public/scripts/controllers/trails-new.controller.js
@@ -0,0 +1,22 @@
+TrailsNewController.$inject = ["$location", "$http"]; // minification protection
+function TrailsNewController ($location, $http) {
+  var vm = this;
+  vm.create = create;
+  vm.trail = {}; // form data
+
+  ////
+
+  function create() {
+    $http
+      .post('/api/trails', vm.trail)
+      .then(onCreateSuccess, onCreateError);
+
+    function onCreateSuccess(response){
+      $location.path('/trails/' + response.data._id)
+    }
+
+    function onCreateError(response){
+      console.error("Failed to create trail", response);
+    }
+  };
+}

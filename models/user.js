ba556337c61ef0385a13ffafcac2d63d26216438
adamrobertreid <adamrobertreid@gmail.com>
2016-05-28 20:43:18 -0700

    added avatar to users schema

--- models/user.js
+++ models/user.js
@@ -8,6 +8,7 @@ var userSchema = new Schema({
   email: { type: String, unique: true, lowercase: true },
   password: { type: String, select: false },
   displayName: String,
+  avatar: String,
   admin: Boolean,
   trail:{
     type: Schema.Types.ObjectId,

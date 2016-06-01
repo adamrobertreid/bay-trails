a8032edde8d0f924995867692171c1293c8aaa0b
adamrobertreid <adamrobertreid@gmail.com>
2016-05-31 18:47:13 -0700

    new custom js for materialize and index views

---
+++ public/scripts/custom.js
@@ -0,0 +1,17 @@
+console.log('custom materialize there');
+
+$(document).ready(function() {
+
+// landing page header
+$('.parallax').parallax();
+
+
+// Initialize collapse button
+$(".button-collapse").sideNav();
+
+
+
+    // selections for search bar results
+$('select').material_select('destroy');
+
+});

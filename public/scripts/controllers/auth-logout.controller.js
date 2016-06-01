75fa1e5ddde9033755170165654462c46ce91de4
adamrobertreid <adamrobertreid@gmail.com>
2016-05-31 18:44:06 -0700

    changed logout to point to homepage after log out

--- public/scripts/controllers/auth-logout.controller.js
+++ public/scripts/controllers/auth-logout.controller.js
@@ -3,6 +3,6 @@ function LogoutController ($location, UserService) {
   UserService
     .logout()
     .then(function onSuccess() {
-        $location.path('/login');
+        $location.path('/');
     });
 }

92af1a1edb0f56dcbdb67a2a4e3eefe9d355550c
adamrobertreid <adamrobertreid@gmail.com>
2016-06-01 09:14:59 -0700

    cleaned up code

--- public/scripts/custom.js
+++ public/scripts/custom.js
@@ -5,13 +5,13 @@ $(document).ready(function() {
 // landing page header
 $('.parallax').parallax();
 
-
 // Initialize collapse button
 $(".button-collapse").sideNav();
 
-
+// Initialize modal on trail submit page
 $('.modal-trigger').leanModal();
-    // selections for search bar results
+
+// selections for search bar results
 $('select').material_select('destroy');
 
 });

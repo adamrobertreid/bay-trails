ed16e8d5d0dc2ab3fd40527a6f974d2319814e1c
adamrobertreid <adamrobertreid@gmail.com>
2016-06-01 23:46:24 -0700

    playing with drop down filters

--- public/scripts/custom.js
+++ public/scripts/custom.js
@@ -15,4 +15,14 @@ $('.modal-trigger').leanModal();
 // selections for search bar results
 $('select').material_select('destroy');
 
+// 
+//   function MainController() {
+//     var vm = this;
+//     console.log('hi im main material controller');
+//     vm.select = {
+//         value: "Option1",
+//         choices: ["Option1", "I'm an option", "This is materialize", "No, this is Patrick."]
+//     };
+//
+// }
 });

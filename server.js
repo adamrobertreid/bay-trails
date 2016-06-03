a8920f59125547c4325b4b8ce129c7b3c6d57ddc
adamrobertreid <adamrobertreid@gmail.com>
2016-06-02 18:24:31 -0700

    app use for bower componets

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 
 // set the static files location /public/img will be /img for users
 app.use(express.static(__dirname + '/public'));
-
+app.use('/vendor', express.static(__dirname + '/bower_components'));
 // log api requests
 app.use(logger('dev'));
 
